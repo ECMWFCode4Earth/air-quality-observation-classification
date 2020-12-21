9281febda0393c073bebd40634b6f07f6b1b1878
MR <miha.razinger@ecmwf.int>
2020-12-21 09:31:31 +0000

    cleanup work on ecmwf branch

--- Milestone1_Importing-datasets-from-OpenAQ/Milestone1_Import_API_OpenAQStation_OpenAQ.py
+++ Milestone1_Importing-datasets-from-OpenAQ/Milestone1_Import_API_OpenAQStation_OpenAQ.py
@@ -5,24 +5,17 @@ Created on Tue Dec  8 12:13:31 2020
 @author: wegia
 """
 
-
-
 import openaq
-
 import pandas as pd
-
 from pandas import json_normalize
-
 from datetime import datetime, date, time, timezone
-
 import numpy as np
-
 import csv
 
 
 # Step 1 Initiatlise pyOpenAQ API
 #
-#  1 There are no edits 
+#  1 There are no edits
 
 print("  STEP 1 ")
 
@@ -36,55 +29,79 @@ api = openaq.OpenAQ()
 print("OpenAQ pyOpenAPI begun")
 
 
-def Milestone1_Get_OpnenAQ_Dataset_Measurement_OpenAQStation_perStation(StationOpenAQ, parameter):
-    
-#Step 1 Choose the measurement country to import and parameter 
-    
-   res1 = api.measurements(location=StationOpenAQ, parameter=parameter, date_to=dt_end, date_from=dt_begin, limit=10000, df=True)
+def Milestone1_Get_OpnenAQ_Dataset_Measurement_OpenAQStation_perStation(
+    StationOpenAQ, parameter
+):
 
-   print("Completed measurements ")
+    # Step 1 Choose the measurement country to import and parameter
 
-   return res1
+    res1 = api.measurements(
+        location=StationOpenAQ,
+        parameter=parameter,
+        date_to=dt_end,
+        date_from=dt_begin,
+        limit=10000,
+        df=True,
+    )
+
+    print("Completed measurements ")
+
+    return res1
 
 
-def Milestone1_Get_Measurements_CSV_OpenAQStation(OpenAQ_Stations, SelectionOpenAQChoose, parameter, SelectionOpenAQ, dt_begin, dt_end):
-    
-   OpenAQDataset = r'OpenAQ_Dataset'
- 
-   SelectedOpenAQ = "Selection"
-   
-   if(SelectionOpenAQ == 0):
-     SelectedOpenAQ = "One Station"
-      
-   if(SelectionOpenAQ == 1):
-     SelectedOpenAQ = "CoordinateCentreandRadius"
-  
-   if(SelectionOpenAQ == 2):
-     SelectedOpenAQ = "Country"
-   
-    
-   OpenAQDataset = OpenAQDataset + " " + SelectionOpenAQChoose + " " + parameter + " " + SelectedOpenAQ + " " +  str(dt_begin) + " to " + str(dt_end) + ".csv" 
-    
-   print("Printed OpenAQ import to ")
-   
-   print( OpenAQDataset)
-   
-   OpenAQ_Stations.to_csv(OpenAQDataset,index=False)                       
+def Milestone1_Get_Measurements_CSV_OpenAQStation(
+    OpenAQ_Stations, SelectionOpenAQChoose, parameter, SelectionOpenAQ, dt_begin, dt_end
+):
+
+    OpenAQDataset = r"OpenAQ_Dataset"
+
+    SelectedOpenAQ = "Selection"
+
+    if SelectionOpenAQ == 0:
+        SelectedOpenAQ = "One Station"
+
+    if SelectionOpenAQ == 1:
+        SelectedOpenAQ = "CoordinateCentreandRadius"
+
+    if SelectionOpenAQ == 2:
+        SelectedOpenAQ = "Country"
+
+    OpenAQDataset = (
+        OpenAQDataset
+        + " "
+        + SelectionOpenAQChoose
+        + " "
+        + parameter
+        + " "
+        + SelectedOpenAQ
+        + " "
+        + str(dt_begin)
+        + " to "
+        + str(dt_end)
+        + ".csv"
+    )
+
+    print("Printed OpenAQ import to ")
+
+    print(OpenAQDataset)
+
+    OpenAQ_Stations.to_csv(OpenAQDataset, index=False)
 
 
 def Milestone1_Print_CSV_OpenAQ_Import(OpenAQ_Dataset):
 
-   OpenAQ_Dataset_Import = "OpenAQ_Dataset"
-    
-   OpenAQ_DatasetCSV = "r'" + OpenAQ_Dataset_Import +  ".csv'" 
-    
-   OpenAQ_Dataset.to_csv(OpenAQ_DatasetCSV,index=False)                       
+    OpenAQ_Dataset_Import = "OpenAQ_Dataset"
+
+    OpenAQ_DatasetCSV = "r'" + OpenAQ_Dataset_Import + ".csv'"
+
+    OpenAQ_Dataset.to_csv(OpenAQ_DatasetCSV, index=False)
 
 
+print(
+    "Get the OpenAQ measurements for stations within raduis of chosen Coordinates from OpenAQ and doing Quality Control on it"
+)
 
-print("Get the OpenAQ measurements for stations within raduis of chosen Coordinates from OpenAQ and doing Quality Control on it")
-
-#Step 3 Choose the measurement country to import and parameter
+# Step 3 Choose the measurement country to import and parameter
 #
 # Choose the station
 #
@@ -92,12 +109,10 @@ print("Get the OpenAQ measurements for stations within raduis of chosen Coordina
 
 
 print("  STEP 2 ")
-
 print("********")
-
 print("Chosen OpenAQ Coordinates Lat/lng and Radius: ")
-     
-OpenAQStation = 'US Diplomatic Post: Abu Dhabi' #Edit
+
+OpenAQStation = "US Diplomatic Post: Abu Dhabi"  # Edit
 
 print(OpenAQStation)
 
@@ -115,46 +130,45 @@ print("********")
 
 print("Parameter chosen")
 
-parameter = 'pm25'
+parameter = "pm25"
 
 print(parameter)
 
 
-#Step 4 Choose time schedule 
+# Step 4 Choose time schedule
 #
-# 
+#
 # 1 Change Time Schedule from 6 months to other in dt_begin
 #  and dt_end
 #
-#  dt_begin =  date(2020,3,1) 1 March 2020 
+#  dt_begin =  date(2020,3,1) 1 March 2020
 #
 #  dt_end =  date(2020,9,1) 1 September 2020
 
-dt_begin = date(2020,3,1) # Edit
+dt_begin = date(2020, 3, 1)  # Edit
 
-dt_end = date(2020,9,1) # Edit
+dt_end = date(2020, 9, 1)  # Edit
 
 print("  STEP 4 ")
 
 print("********")
 
 
-print("Getting OpenAQ dataset applying pyOpenAQ API from ") 
+print("Getting OpenAQ dataset applying pyOpenAQ API from ")
 print(dt_begin)
 print(" to ")
 print(dt_end)
 print(" for one OpenAQ Station and one parameter ")
 
 
+# Step 5 Get Measurements from openAQ API
+#
+#
+# Test
+#
+# resp = api.cities(df=True, limit=10000)
 
-# Step 5 Get Measurements from openAQ API 
-#
-#
-# Test 
-#
-#resp = api.cities(df=True, limit=10000)
-
-#res1 = api.measurements(city='Delhi', df=True, limit=10000)
+# res1 = api.measurements(city='Delhi', df=True, limit=10000)
 
 print("  STEP 5 ")
 
@@ -162,18 +176,20 @@ print("********")
 
 print("Getting Measurements from OpenAQ API source")
 
-res2 = Milestone1_Get_OpnenAQ_Dataset_Measurement_OpenAQStation_perStation(OpenAQStation, parameter)
+res2 = Milestone1_Get_OpnenAQ_Dataset_Measurement_OpenAQStation_perStation(
+    OpenAQStation, parameter
+)
 
 print("Found these Stations in Coordinates")
 
-OpenAQStationunique = res2['location'].unique()
+OpenAQStationunique = res2["location"].unique()
 
 print(OpenAQStationunique)
 
 
 # Step 6 Get Measurements CSV
-#  
-#  Choose unique selection 
+#
+#  Choose unique selection
 #
 #  Change   SelectionOpenAQChoose = " "
 
@@ -188,4 +204,6 @@ SelectionOpenAQ = 0
 
 SelectionOpenAQChoose = "Unique selection"
 
-Milestone1_Get_Measurements_CSV_OpenAQStation(res2, SelectionOpenAQChoose, parameter, SelectionOpenAQ, dt_begin, dt_end)
+Milestone1_Get_Measurements_CSV_OpenAQStation(
+    res2, SelectionOpenAQChoose, parameter, SelectionOpenAQ, dt_begin, dt_end
+)

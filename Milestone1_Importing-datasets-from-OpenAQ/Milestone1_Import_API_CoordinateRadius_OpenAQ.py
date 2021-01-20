065250948edee94f0dde18ba8668aedeee6a658a
Gordon <wegiangb@hotmail.co.uk>
2021-01-20 12:51:02 +0000

    Add files via upload

--- Milestone1_Importing-datasets-from-OpenAQ/Milestone1_Import_API_CoordinateRadius_OpenAQ.py
+++ Milestone1_Importing-datasets-from-OpenAQ/Milestone1_Import_API_CoordinateRadius_OpenAQ.py
@@ -15,6 +15,8 @@ from pandas import json_normalize
 
 from datetime import datetime, date, time, timezone
 
+import time
+
 import numpy as np
 
 import csv
@@ -32,27 +34,140 @@ print("Initialise pyOpenAQ API")
 
 api = openaq.OpenAQ()
 
+MAX_RETRIES = 10
 
 print("OpenAQ pyOpenAPI begun")
 
-def Milestone1_Get_OpnenAQ_Dataset_Measurement_perStation(StationOpenAQCoordinates, Radius, parameter, dt_begin, dt_end):
+
+
+def Milestone1_notGet_OpenAQ_Dataset_Measurement_perStation(StationOpenAQCoordinates, Radius, parameter, dt_begin, dt_end):
     
 #Step 1 Choose the measurement coordinate centre radius to import and parameter 
-   
-   if(len(parameter) == 1):
-       
-      res1 = api.measurements(coordinates=StationOpenAQCoordinates, parameter=parameter[0], radius=Radius, date_to=dt_end, date_from=dt_begin, df=True, limit=10000)
+    
+   res1 = api.measurements(coordinates=StationOpenAQCoordinates, parameter='pm25', radius=Radius, date_to=dt_end, date_from=dt_begin, df=True, limit=10000)
   
-   else:  
-   
-      res1 = api.measurements(coordinates=StationOpenAQCoordinates, radius=Radius, date_to=dt_end, date_from=dt_begin, df=True, limit=10000)
-      
    
    print("Completed measurements ")
 
    return res1
 
 
+
+def Milestone1_Get_OpenAQ_Dataset_Measurement_perStation(StationOpenAQCoordinates, Radius, parameter, dt_begin, dt_end):
+    
+#Step 1 Choose the measurement coordinate centre radius to import and parameter 
+  
+
+   res1 = []
+   OpenAQStation = []
+   
+   for ii in range(1, MAX_RETRIES + 1):
+
+      try: 
+    
+         if(len(parameter) == 1):
+       
+           OpenAQStation = api.measurements(
+               coordinates=StationOpenAQCoordinates, 
+               radius=Radius,
+               parameter=parameter[0], 
+               date_to=dt_end, 
+               date_from=dt_begin, 
+               index="utc",
+               df=True, 
+               limit=10000
+               )
+           
+      #     res1 = []
+             
+           res1.append(OpenAQStation)
+             
+         else:  
+     
+           res1 = Milestone1_Get_OpenAQ_Dataset_Measurementexception_perStation(StationOpenAQCoordinates, Radius, parameter, dt_begin, dt_end)    
+         
+          #  res1 = api.measurements(coordinates=StationOpenAQCoordinates, radius=Radius, date_to=dt_end, date_from=dt_begin, df=True, limit=10000)
+            
+   
+          #  res1 = api.measurements(
+          #     coordinates=StationOpenAQCoordinates,
+          #     radius=Radius, 
+          #     date_to=dt_end, 
+          #     date_from=dt_begin, 
+          #     df=True, 
+          #     limit=10000
+          #     )
+      
+      except openaq.exceptions.ApiError:
+            time.sleep(5)
+            continue
+      else:
+            break
+    
+  # OpenAQStation.append(res1)
+   
+   print("Completed measurements ")
+
+   return res1
+  
+
+def Milestone1_Get_OpenAQ_Dataset_Measurementexception_perStation(StationOpenAQCoordinates, Radius, parameter, dt_begin, dt_end):
+    
+   OpenAQStations = []
+   iteration = 0
+   
+   status = 0
+   
+   parameter_selection = ['pm25','bc','co','o3','pm10','pm25','so2']
+ 
+   #for parameters in parameter_selection:
+       
+   try:
+       # print(parameters)
+         
+         
+       OpenAQselection = "coordinates='" + StationOpenAQCoordinates + "', radius='" + str(Radius) + "', parameter='" 
+       
+       #+ "', date_to=" + str(dt_end) + " , date_from=" + str(dt_begin) + " , df=True" +  " , index=" + "utc" + " , limit= 10000" 
+         
+       print(OpenAQselection)
+         
+       res1 = api.measurements(coordinates=StationOpenAQCoordinates, radius=Radius,date_to=dt_end, date_from=dt_begin, index="utc", df=True, limit=10000)
+            
+       #  print(res1)
+         
+         
+         
+          #  res1 = api.measurements(
+          #     coordinates=StationOpenAQCoordinates,
+          #     radius=Radius, 
+          #     date_to=dt_end, 
+          #     date_from=dt_begin, 
+          #     df=True, 
+          #     limit=10000
+          #     )
+      
+       OpenAQStations.append(res1)  
+      
+   except openaq.exceptions.ApiError:
+       time.sleep(5)
+       OpenAQStations.append([])
+       pass
+   else:
+       print(" Exception ")
+       pass
+    
+       
+    #  print(status)
+         
+         
+   iteration = iteration + 1
+   
+   print(OpenAQStations[0])   
+   
+   return OpenAQStations  
+          
+          
 
 def Milestone1_Get_Measurements_CSV_OpenAQStation(OpenAQ_Stations, SelectionOpenAQChoose, parameter, SelectionOpenAQ, dt_begin, dt_end):
     
@@ -69,15 +184,25 @@ def Milestone1_Get_Measurements_CSV_OpenAQStation(OpenAQ_Stations, SelectionOpen
    if(SelectionOpenAQ == 2):
      SelectedOpenAQ = "Country"
    
-    
-   OpenAQDataset = OpenAQDataset + " " + SelectionOpenAQChoose + " " + parameter + " " + SelectedOpenAQ + " " +  str(dt_begin) + " to " + str(dt_end) + ".csv" 
-    
-   print("Printed OpenAQ import to ")
+   iteration = 0 
    
-   print( OpenAQDataset)
    
-   OpenAQ_Stations.to_csv(OpenAQDataset,index=False)                       
-
+   
+   for parameters in OpenAQ_Stations:
+   
+      parameter_selections = OpenAQ_Stations[iteration]['parameter'].unique()   
+   
+      OpenAQDatasetcomplete = OpenAQDataset + " " + SelectionOpenAQChoose + " " + parameter_selections[0] + " " + SelectedOpenAQ + " " +  str(dt_begin) + " to " + str(dt_end) + ".csv" 
+    
+      print("Printed OpenAQ import to ")
+   
+      print( OpenAQDatasetcomplete)
+   
+    
+      OpenAQ_Stations[iteration].to_csv(OpenAQDatasetcomplete,index=False)                       
+    
+      iteration = iteration + 1
+     
 
 def Milestone1_Print_CSV_OpenAQ_Import(OpenAQ_Dataset):
 
@@ -107,9 +232,12 @@ print("Chosen OpenAQ Coordinates Lat/lng and Radius: ")
     
  
      
-OpenAQStationCoordinates = "-34.60638,-58.43194" #Edit Lat, Lng
+OpenAQStationCoordinates = "34.60638,58.43194" # "24.4244,54.43375"
 
-Radius = 25000 # Edit in metres 
+
+#"-34.60638,-58.43194" #Edit Lat, Lng
+
+Radius = 2500000 # Edit in metres 
 
 print(OpenAQStationCoordinates)
 
@@ -137,12 +265,12 @@ SelectEveryParameter_YesorNo = 1 # Edit 1 Yes 0 No just one
 
 parameter = 'pm25'  # Edit
 
-parameter_selection = []
-
 print("Parameter chosen")
 
 if(SelectEveryParameter_YesorNo == 0):
     
+   parameter_selection = []
+
    parameter_selection.append(parameter)
 
    print(parameter_selection)
@@ -151,7 +279,7 @@ if(SelectEveryParameter_YesorNo == 1):
     
    print("Every parameter at selected or chosen OpenAQ station") 
 
-
+   parameter_selection = ['bc','co','no2','o3','pm10','pm25','so2']
 
 print("Completed Step 3 ")
 
@@ -170,7 +298,7 @@ print(">")
 
 dt_begin = date(2020,3,1) # Edit
 
-dt_end = date(2020,9,1) # Edit
+dt_end = date(2020,6,1) # Edit
 
 print("  STEP 4 ")
 
@@ -206,13 +334,29 @@ print("********")
 
 print("Getting Measurements from OpenAQ API source")
 
-res2 = Milestone1_Get_OpnenAQ_Dataset_Measurement_perStation(OpenAQStationCoordinates, Radius, parameter_selection, dt_begin, dt_end)
+#res2 = Milestone1_notGet_OpnenAQ_Dataset_Measurement_perStation(OpenAQStationCoordinates, Radius, parameter_selection, dt_begin, dt_end)
+
+res2 = Milestone1_Get_OpenAQ_Dataset_Measurement_perStation(OpenAQStationCoordinates, Radius, parameter_selection, dt_begin, dt_end)
 
 print("Found these Stations in Coordinates")
 
-OpenAQStationunique = res2['location'].unique()
+if(len(res2[0]) > 0):
 
-print(OpenAQStationunique)
+  OpenAQStationunique = res2[0]['location'].unique()
+
+  print(OpenAQStationunique)
+
+if(len(res2[0]) == 0):
+  
+  print("*****") 
+  
+  print("API Error")
+    
+  print("The API returned an error because it was unavailable after trying this many requests ")  
+  
+  print(MAX_RETRIES)
+  
+  print("Try to the process again after a few minutes")
 
 print("Completed Step 5 ")
 
@@ -235,7 +379,9 @@ SelectionOpenAQ = 1
 
 SelectionOpenAQChoose = "Unique selection 24.4244 54.43375"
 
-Milestone1_Get_Measurements_CSV_OpenAQStation(res2, SelectionOpenAQChoose, parameter, SelectionOpenAQ, dt_begin, dt_end)
+if(len(res2[0]) > 0):
+
+    Milestone1_Get_Measurements_CSV_OpenAQStation(res2, SelectionOpenAQChoose, parameter_selection, SelectionOpenAQ, dt_begin, dt_end)
 
 
 print("Completed Step 6 ")
